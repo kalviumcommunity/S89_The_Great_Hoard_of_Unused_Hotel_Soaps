27ccf06b3a5a9c92156803fc7607bb53ec33ecb6
Papan07 <papanns19@gmail.com>
2025-03-19 15:47:37 +0530

    update

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 
-// Define a simple /ping route
-app.get("/ping", (req, res) => {
-    res.json({ message: "Pong!" });
+// Define the /ping route
+app.get('/ping', (req, res) => {
+  res.send('Pong!');
 });
 
-// Start the server
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Start the server with error handling
+app.listen(8080,async()=>{
+  try {
+    await mongoose.connect("mongodb+srv://papanns19:Papan7890@cluster0.3pp3u.mongodb.net/")
+    console.log("server connected sucessfully")
+  } catch (error){
+    console.log(error)
+  }
+})
