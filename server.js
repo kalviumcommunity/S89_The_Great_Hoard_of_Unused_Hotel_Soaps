9e864f4eda2910016c6d4d7bdea154a7f698b6e1
Papan07 <papanns19@gmail.com>
2025-03-22 10:56:03 +0530

    update

--- server.js
+++ server.js
@@ -2,33 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-
+const dotenv = require('dotenv');
+dotenv.config();
 // Define the /ping route
 app.get('/ping', (req, res) => {
   res.send('Pong!');
 });
 
 
-// 404 handler
-app.use((req, res) => {
-    res.status(404).json({ error: "Route not found" });
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Internal Server Error" });
-});
-
-// Start the server
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('DB connected successfully!');
 });
 // Start the server with error handling
 app.listen(8080,async()=>{
   try {
-    await mongoose.connect("mongodb+srv://papanns19:Papan7890@cluster0.3pp3u.mongodb.net/")
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("server connected sucessfully")
   } catch (error){
     console.log(error)
